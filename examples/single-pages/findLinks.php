2229741ec99064447e458bda061ba9d8cf2dc68c
Erik Espana <erikespana@users.noreply.github.com>
2018-12-07 08:50:21 -0600

    Renamed $url parameter to $file.
    Example: findLinks.php?file=https://www.austinchronicle.com

--- examples/single-pages/findLinks.php
+++ examples/single-pages/findLinks.php
@@ -1,5 +1,25 @@
+<?php
+/*
+	findLinks.php
+	
+	Finds all the hyperlinks in a file. File can be a URL.
+*/
+
+
+// Checks if script was passed a "file" parameter.
+if ( empty( $_GET["file"] ) ) {
+	
+	$file = "tcptsec07c.html";
+	//$file = "https://www.tdi.texas.gov/title/tcptsec07a.html";
+
+} else {
+	// Scrape the file passed as a parameter.
+	$file = $_GET["file"];
+}
+?>
 <html>
 <head>
+	<link href="https://www.tdi.texas.gov/_global/css/styles.css" rel="stylesheet"/>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.1/jquery.min.js" type="text/javascript"></script>
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
 	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
@@ -7,7 +27,7 @@
 </head>
 <header>
 	<div class="one-row">
-		<div class="col6">List of hrefs</div>
+		<div class="col6"><a href="https://www.tdi.texas.gov/"><img alt="Texas Department of Insurance" class="logonew" src="https://www.tdi.texas.gov/_global/images/tdi-logo-new-web.png"></a></div>
 	</div>
 </header>
 <main class="standard wrapper">
@@ -15,23 +35,33 @@
 	
 	<section class="mainContent ">
 
+<p>Searching: <?php echo $file ?></p>
+
 	<table id="table_id" class="display">
 		<thead>
 			<tr>
 				<th>Match</th>
-				<th>Linked Text</th>
 				<th>Link</th>
+				<th>Linked Text</th>
 			</tr>
 		</thead>
 		<tbody>
 <?php
-$file = "file.html";
+
 // include class
-include_once "htmlParser.php";
-$obj = new htmlParser($file);
-$obj->getHTML();
+include_once "scrape-web-pages/htmlParser.php";
+$obj = new htmlParser();
+
+// Saves contents of $file as a string, in class property: $this->html.
+$obj->getHTML($file);
+
+// List all the links in $file,
+// matched by a regular expression (class: $patternAHref).
 $obj->reportLinksWithLineNumbers();
+
+// Debugging: Prints the contents of the class's $html variable.
 //$obj->showHTML();
+
 ?>
 		</tbody>
 	</table>
